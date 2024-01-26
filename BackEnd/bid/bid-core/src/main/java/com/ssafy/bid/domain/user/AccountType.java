6fd2870accb29c66a0f508c03e4db17f511a1a15
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-26 22:53:49 +0900

    [S10P12A306-212] chore: Add entities

---
+++ BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/domain/user/AccountType.java
@@ -0,0 +1,15 @@
+package com.ssafy.bid.domain.user;
+
+import lombok.Getter;
+
+@Getter
+public enum AccountType {
+	EXPENDITURE("지출"),
+	INCOME("수입");
+
+	private final String accountType;
+
+	private AccountType(String accountType) {
+		this.accountType = accountType;
+	}
+}
