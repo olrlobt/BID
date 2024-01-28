807a7c60b6a9408c6b2fad5793c7864771f254df
Lainlnya <kimyelim100@gmail.com>
2024-01-29 01:23:55 +0900

    feat: add Total Class Page

---
+++ src/Page/ClassManage/Teacher.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Logo from '../../Asset/Image/logo.png';
+import styled from './Teacher.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGear,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function Teacher() {
+  const location = useLocation();
+
+  return (
+    <section className={styled.teacherArea}>
+      <img src={Logo} alt="로고" />
+      <section>
+        <div className={styled.eleInfo}>해오름초등학교</div>
+        <div className={styled.nameInfo}>
+          김비드<span>선생님</span>
+        </div>
+        <section className={styled.teacherBtn}>
+          {/* 현재 url에 따라 어떤 정보 보여줄지 표현 */}
+          {location.pathname === '/classlist/:teacherId/' ? (
+            <Link to="/classlist/:teacherId/modify" className={styled.classBtn}>
+              <FontAwesomeIcon className={styled.icon} icon={faGear} />
+              계정/학급관리
+            </Link>
+          ) : (
+            <Link
+              to="/classlist/:teacherId/modify"
+              className={`${styled.classBtn} ${
+                location.pathname !== '/classlist/:teacherId/'
+                  ? styled.disabled
+                  : ''
+              }`}
+            >
+              비밀번호 변경
+            </Link>
+          )}
+          {location.pathname === '/classlist/:teacherId/' ? (
+            <button className={styled.logoutBtn}>
+              <FontAwesomeIcon
+                className={styled.icon}
+                icon={faArrowRightFromBracket}
+              />
+              로그아웃
+            </button>
+          ) : (
+            <Link to="/delete/:id/" className={styled.logoutBtn}>
+              회원탈퇴
+            </Link>
+          )}
+        </section>
+      </section>
+    </section>
+  );
+}
--- src/Page/User/DeletePage.jsx
+++ src/Page/User/DeletePage.jsx
@@ -1,26 +1,29 @@
-import React from "react";
-import styled from "./DeletePage.module.css";
-import { Link } from "react-router-dom";
-import Logo from "../../Component/Common/Logo";
+import React from 'react';
+import styled from './DeletePage.module.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../Component/Common/Logo';
 
-
-function RegisterPage() {
+export default function DeletePage() {
   return (
     <section className={styled.back}>
       <div className={styled.logo}>
-      <Logo text="회원 탈퇴" />
+        <Logo text="회원 탈퇴" />
       </div>
-      <div className={styled.content}>
-        <div className={styled.contentInput}>
-          정말 탈퇴할 거니..?
-          <input type="password" placeholder="비밀번호" />
-          <Link to="/login" className={styled.IdLink}>
-                <p>회원 탈퇴</p>
-              </Link>
-        </div>
+      <div className={styled.contentInput}>
+        <span>
+          정말 탈퇴하실 건가요? <br /> 회원 탈퇴를 하시면 데이터를 복구할 수
+          없어요
+        </span>
+        <input
+          className={styled.enterPW}
+          type="password"
+          placeholder="비밀번호"
+        />
+
+        <Link to="/login" className={styled.IdLink}>
+          <p>회원 탈퇴</p>
+        </Link>
       </div>
     </section>
   );
 }
-
-export default RegisterPage;
