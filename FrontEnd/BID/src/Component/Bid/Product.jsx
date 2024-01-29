0fe6a9f88df803ebce5b6a11a39094a887020800
김예림 <kimyelim100@naver.com>
2024-01-29 19:53:45 +0900

    [S10P12A306-528] fix: eslint code for frontend CI/CD

--- FrontEnd/BID/src/Component/Bid/Product.jsx
+++ FrontEnd/BID/src/Component/Bid/Product.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import styled from "./Product.module.css";
 
-export default function Product(props){
-  const {no, title, contents, imgUrl, goodsType, goodsName, userName, startPrice, avgPrice, endTime, createdAt} = props;
+export default function Product(props) {
+  const {
+    // no,
+    title,
+    // contents,
+    imgUrl,
+    // goodsType,
+    // goodsName,
+    userName,
+    // startPrice,
+    avgPrice,
+    // endTime,
+    // createdAt,
+  } = props;
 
-  return(
-    <div className = {styled.productWrapper}>
-      <div className = {styled.imgWrapper}>
-        <img src = {imgUrl}/>
+  return (
+    <div className={styled.productWrapper}>
+      <div className={styled.imgWrapper}>
+        <img src={imgUrl} alt="이미지" />
       </div>
-      <div className = {styled.productHeader}>
-        <span className = {styled.title}>{title}</span>
-        <span className = {styled.userName}>{userName}</span>
+      <div className={styled.productHeader}>
+        <span className={styled.title}>{title}</span>
+        <span className={styled.userName}>{userName}</span>
       </div>
-      <div className = {styled.avgPrice}>{avgPrice}비드</div>
+      <div className={styled.avgPrice}>{avgPrice}비드</div>
     </div>
   );
 }
--- FrontEnd/BID/src/Component/Common/TimeTable.jsx
+++ FrontEnd/BID/src/Component/Common/TimeTable.jsx
@@ -1,19 +1,19 @@
-import Calender from '../../Asset/Image/HOME_icons/calender-dynamic-color.png';
-import styled from './TimeTable.module.css';
-import React from 'react';
+// import Calender from '../../Asset/Image/HOME_icons/calender-dynamic-color.png';
+import styled from "./TimeTable.module.css";
+import React from "react";
 
 export default function TimeTable() {
   const startH = [
-    '09:00',
-    '10:00',
-    '11:00',
-    '12:00',
-    '13:00',
-    '14:00',
-    '15:00',
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
   ];
 
-  const endH = ['09:40', '10:40', '11:40', '12:40', '13:40', '14:40', '15:40'];
+  const endH = ["09:40", "10:40", "11:40", "12:40", "13:40", "14:40", "15:40"];
   return (
     <section className={styled.timeTable}>
       <div className={styled.timeImage}></div>
@@ -28,23 +28,10 @@ export default function TimeTable() {
               <span>-</span>
               <span>{endH[index]}</span>
             </div>
-            {index == startH.length - 1 ? '' : <hr />}
+            {index === startH.length - 1 ? "" : <hr />}
           </React.Fragment>
         ))}
       </section>
     </section>
   );
-}
-{
-  /* <select id="current">
-  {hour.map((value) => (
-    <option value>{value}</option>
-  ))}
-</select>
-<span> : </span>
-<select id="current">
-  {time.map((value) => (
-    <option value>{value}</option>
-  ))}
-</select> */
 }
