6e7982cf59baadc7eeb6dfccf999b90b82df7863
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-01-30 18:19:29 +0900

    refactor: Remove nonused codes

--- FrontEnd/BID/src/Component/Bid/CouponList.jsx
+++ FrontEnd/BID/src/Component/Bid/CouponList.jsx
@@ -1,22 +1,22 @@
-import React from "react"; // { useEffect, useMemo, useState }
-import styled from "./CouponList.module.css";
+import React, { useEffect, useMemo, useState } from "react";
+import styled from "./CouponList.module.css"
 import Coupon from "./Coupon";
 
-export default function CouponList(props) {
-  const { coupons } = props;
-
-  return (
-    <div className={styled.couponListWrapper}>
-      {coupons.map((coupon) => (
-        <Coupon
-          key={coupon.no}
-          no={coupon.no}
-          name={coupon.name}
-          description={coupon.description}
-          startPrice={coupon.startPrice}
-          coupons={coupons}
-        />
-      ))}
-    </div>
-  );
+export default function CouponList(props){
+	const { coupons } = props;
+	
+	return(
+		<div className = {styled.couponListWrapper}>
+			{
+				coupons.map((coupon) =>
+					<Coupon
+						key = {coupon.no}
+						no = {coupon.no}
+						name = {coupon.name}
+						description = {coupon.description}
+						startPrice = {coupon.startPrice}
+					/>)
+			}
+		</div>
+	);
 }
