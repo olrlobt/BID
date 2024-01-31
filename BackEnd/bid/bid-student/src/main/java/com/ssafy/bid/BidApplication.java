5db6dd1e26c8acfa1247c2b6a6eb6174fc0d2af6
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-31 14:05:06 +0900

    [S10P12A306-440] chore: Add required settings

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/BidApplication.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/BidApplication.java
@@ -2,7 +2,9 @@ package com.ssafy.bid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BidApplication {
 
