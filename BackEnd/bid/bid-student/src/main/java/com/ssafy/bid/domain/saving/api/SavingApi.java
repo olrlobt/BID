fa8e58c2c4c81aa5d9b9023f77154f68faf52824
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-31 15:42:51 +0900

    [S10P12A306-441] feat: Add student saveSaving api

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/api/SavingApi.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/api/SavingApi.java
@@ -2,9 +2,13 @@ package com.ssafy.bid.domain.saving.api;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.bid.domain.saving.dto.SavingRequest;
 import com.ssafy.bid.domain.saving.dto.SavingsResponse;
 import com.ssafy.bid.domain.saving.service.SavingService;
 
@@ -20,5 +24,12 @@ public class SavingApi {
 	public List<SavingsResponse> findSavings() {
 		//TODO: SecurityUser 설정 후, @AuthenticationPrincipal 로 로그인 정보 내에서 학급pk 가져와서 파라미터로 넘겨야 함
 		return savingService.findSavings(1);
+	}
+
+	@PostMapping("/savings")
+	public ResponseEntity<Void> saveSavings(SavingRequest savingRequest) {
+		//TODO: SecurityUser 설정 후, @AuthenticationPrincipal 로 로그인 정보 내에서 학급pk 가져와서 파라미터로 넘겨야 함
+		savingService.saveSaving(2, savingRequest);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }
--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/service/SavingService.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/service/SavingService.java
@@ -2,8 +2,11 @@ package com.ssafy.bid.domain.saving.service;
 
 import java.util.List;
 
+import com.ssafy.bid.domain.saving.dto.SavingRequest;
 import com.ssafy.bid.domain.saving.dto.SavingsResponse;
 
 public interface SavingService {
 	List<SavingsResponse> findSavings(int gradeNo);
+
+	void saveSaving(int userNo, SavingRequest savingRequest);
 }
