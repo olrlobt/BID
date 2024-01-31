f8c2370330aff0aeac60ab95cb168a6e85c8831b
백지윤 <wendy6992@naver.com>
2024-01-31 17:52:21 +0900

    [S10P12A306-555] fix: Eslint code

--- FrontEnd/BID/src/Component/Common/Button2.jsx
+++ FrontEnd/BID/src/Component/Common/Button2.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "./Button2.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SvgIcon } from "@material-ui/core";
 import SettingsIcon from '@mui/icons-material/Settings';
 
