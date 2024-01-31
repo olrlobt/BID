47545fc3980b682b594e242d0347713d8ef12eb4
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-31 15:42:51 +0900

    [S10P12A306-441] feat: Add student saveSaving api

---
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/repository/UserSavingRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.bid.domain.saving.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.bid.domain.saving.UserSaving;
+
+public interface UserSavingRepository extends JpaRepository<UserSaving, Integer> {
+}
--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/service/SavingServiceImpl.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/service/SavingServiceImpl.java
@@ -3,20 +3,33 @@ package com.ssafy.bid.domain.saving.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bid.domain.saving.UserSaving;
+import com.ssafy.bid.domain.saving.dto.SavingRequest;
 import com.ssafy.bid.domain.saving.dto.SavingsResponse;
 import com.ssafy.bid.domain.saving.repository.SavingRepository;
+import com.ssafy.bid.domain.saving.repository.UserSavingRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class SavingServiceImpl implements SavingService {
 
 	private final SavingRepository savingRepository;
+	private final UserSavingRepository userSavingRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<SavingsResponse> findSavings(int gradeNo) {
 		return savingRepository.findSavings(gradeNo);
+	}
+
+	@Override
+	public void saveSaving(int userNo, SavingRequest savingRequest) {
+		UserSaving userSaving = savingRequest.toEntity(userNo);
+		userSavingRepository.save(userSaving);
 	}
 }
