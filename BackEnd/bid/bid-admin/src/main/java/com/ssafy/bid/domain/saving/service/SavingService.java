dfbdeabae4a441fc4cb31c355451c98273203929
서울_17반_이현진 <hgene0929@gmail.com>
2024-01-31 09:36:53 +0900

    [S10P12A306-389] feat: Add admin modifySavings api

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/saving/service/SavingService.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/saving/service/SavingService.java
@@ -2,8 +2,11 @@ package com.ssafy.bid.domain.saving.service;
 
 import java.util.List;
 
+import com.ssafy.bid.domain.saving.dto.SavingModifyRequest;
 import com.ssafy.bid.domain.saving.dto.SavingsResponse;
 
 public interface SavingService {
-	public List<SavingsResponse> findSavings(int gradeNo);
+	List<SavingsResponse> findSavings(int gradeNo);
+
+	void modifySavings(int gradeNo, SavingModifyRequest savingModifyRequest);
 }
--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/saving/service/SavingServiceImpl.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/saving/service/SavingServiceImpl.java
@@ -3,20 +3,37 @@ package com.ssafy.bid.domain.saving.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bid.domain.saving.Saving;
+import com.ssafy.bid.domain.saving.dto.SavingModifyRequest;
+import com.ssafy.bid.domain.saving.dto.SavingRequest;
 import com.ssafy.bid.domain.saving.dto.SavingsResponse;
 import com.ssafy.bid.domain.saving.repository.SavingRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class SavingServiceImpl implements SavingService {
 
 	private final SavingRepository savingRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<SavingsResponse> findSavings(int gradeNo) {
 		return savingRepository.findSavings(gradeNo);
+	}
+
+	@Override
+	public void modifySavings(int gradeNo, SavingModifyRequest savingModifyRequest) {
+		List<Saving> savings = savingRepository.findAllByGradeNo(gradeNo);
+		List<SavingRequest> savingRequests = savingModifyRequest.getSavingRequests();
+
+		savings.forEach(saving -> savingRequests.stream()
+			.filter(request -> request.getNo() == saving.getNo())
+			.forEach(request -> saving.modify(request.getDepositPrice(), request.getInterestRate()))
+		);
 	}
 }
