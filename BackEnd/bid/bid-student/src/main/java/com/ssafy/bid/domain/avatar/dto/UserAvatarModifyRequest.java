bc0a145362898b232d7a9b59bc5dcfc6b37b98a6
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-02 00:40:45 +0900

    [S10P12A306-639] feat: Add modifyUserAvatar api

---
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/avatar/dto/UserAvatarModifyRequest.java
@@ -0,0 +1,10 @@
+package com.ssafy.bid.domain.avatar.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserAvatarModifyRequest {
+	private String url;
+}
