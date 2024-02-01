f52876668128199e54bd2bed5dfe9b4c86d4f91f
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-01 17:24:03 +0900

    [S10P12A306-630] feat: Add sendReward api

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/reward/service/RewardServiceImpl.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/reward/service/RewardServiceImpl.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bid.domain.notification.NotificationType;
+import com.ssafy.bid.domain.notification.dto.NotificationRequest;
+import com.ssafy.bid.domain.notification.service.NotificationService;
 import com.ssafy.bid.domain.reward.Reward;
 import com.ssafy.bid.domain.reward.dto.RewardSaveRequest;
+import com.ssafy.bid.domain.reward.dto.RewardSendRequest;
 import com.ssafy.bid.domain.reward.dto.RewardsFindResponse;
 import com.ssafy.bid.domain.reward.repository.RewardRepository;
+import com.ssafy.bid.domain.user.Student;
+import com.ssafy.bid.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +24,8 @@ import lombok.RequiredArgsConstructor;
 public class RewardServiceImpl implements RewardService {
 
 	private final RewardRepository rewardRepository;
+	private final UserRepository userRepository;
+	private final NotificationService notificationService;
 
 	@Override
 	public void saveReward(int gradeNo, RewardSaveRequest rewardSaveRequest) {
@@ -34,5 +42,27 @@ public class RewardServiceImpl implements RewardService {
 	@Override
 	public void deleteReward(int rewardNo) {
 		rewardRepository.deleteById(rewardNo);
+	}
+
+	@Override
+	public void sendReward(RewardSendRequest rewardSendRequest) {
+		List<Student> students = userRepository.findAllByIds(rewardSendRequest.getUsersNos());
+
+		if (students.size() != rewardSendRequest.getUsersNos().size()) {
+			throw new IllegalArgumentException("전송할 대상 중 확인되지 않은 사용자 존재"); //TODO: 커스텀 예외처리
+		}
+
+		students.forEach(
+			student -> {
+				student.addRewardPrice(rewardSendRequest.getPrice());
+				NotificationRequest notificationRequest = NotificationRequest.builder()
+					.receiverNo(student.getNo())
+					.title(rewardSendRequest.getName())
+					.content(rewardSendRequest.getComment())
+					.notificationType(NotificationType.REWARD)
+					.build();
+				notificationService.send(notificationRequest);
+			}
+		);
 	}
 }
