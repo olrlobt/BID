72e03748a540bf1187fd60bb8099bb8d85de911b
김예림 <kimyelim100@naver.com>
2024-02-01 17:29:57 +0900

    [S10P12A306-422] feat: Reflecting The Order of Seat Selection [S10P12A306-419] feat: Pick Random Seat for Student

--- FrontEnd/BID/src/Page/Manage/SeatGame.jsx
+++ FrontEnd/BID/src/Page/Manage/SeatGame.jsx
@@ -1,13 +1,58 @@
 import styled from "./SeatGame.module.css";
 import Back from "../../Asset/Image/SeatGame/back_btn.png";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useBalls from "../../hooks/useBalls";
+import html2canvas from "html2canvas";
 
 export default function SeatGame() {
   const navigate = useNavigate();
   const location = useLocation();
   const ballList = location.state;
+
+  // 캡쳐
+  const captureRef = useRef(null);
+
+  /** 캡쳐 버튼 눌렀을 때 작동하는 함수 */
+  const handleCapture = () => {
+    if (captureRef.current) {
+      html2canvas(captureRef.current).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const blob = dataURLtoBlob(imgData);
+        const file = new File([blob], "captured_image.png", {
+          type: "image/png",
+        });
+        saveAs(file, "captured_image.png");
+        console.log(imgData);
+      });
+    }
+  };
+
+  /** Data URL을 Blob 객체로 변환하는 함수 */
+  const dataURLtoBlob = (dataURL) => {
+    const parts = dataURL.split(";base64,");
+    const contentType = parts[0].split(":")[1];
+    const raw = window.atob(parts[1]);
+    const array = new Uint8Array(raw.length);
+
+    for (let i = 0; i < raw.length; i++) {
+      array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([array], { type: contentType });
+  };
+
+  /** Blob을 파일로 저장하는 함수 */
+  const saveAs = (blob, fileName) => {
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fileName;
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+  };
 
   // 공 count만큼 개수 넣기
   const tempList = [];
@@ -20,8 +65,14 @@ export default function SeatGame() {
   const [isBallOpen, setIsBallOpen] = useState(false);
   const [pickList, setPickList] = useState(tempList);
   const [studentName, setStudentName] = useState("");
+  const [seatIndex, setSeatIndex] = useState(0);
 
-  // const {removeBalls} = useBalls();
+  // 자리배정을 위한 배열
+  const [seatList, setSeatList] = useState(
+    Array.from({ length: ballList.length }, () => null)
+  );
+
+  const { removeBalls } = useBalls();
 
   /** 지팡이 클릭했을 때 발생하는 이벤트 */
   const handleCaneClick = () => {
@@ -30,7 +81,7 @@ export default function SeatGame() {
     setTimeout(() => {
       setIsBallOpen(true);
       setIsCaneClicked(false);
-      pickSeat();
+      pickSeat(seatIndex);
 
       setTimeout(() => {
         setIsBallOpen(false);
@@ -44,6 +95,8 @@ export default function SeatGame() {
       alert("더 이상 뽑을 학생이 없습니다.");
       setIsBallOpen(false);
       // redux에 상태 업데이트 해주기
+      removeBalls();
+      return;
     }
 
     // 무작위 하나 선택 후 동일한 숫자를 가진 항목 제거
@@ -55,14 +108,30 @@ export default function SeatGame() {
       ballList.filter((student) => student.no === selectedNum)[0].name
     );
 
+    setSeatList((prevSeatList) => {
+      const updatedList = [...prevSeatList];
+      updatedList[seatIndex] = ballList.find(
+        (student) => student.no === selectedNum
+      ).name;
+      return updatedList;
+    });
+
     setPickList(pickList.filter((num) => num !== selectedNum));
+    setSeatIndex(seatIndex + 1);
   };
 
   return (
-    <section className={styled.seatGame}>
+    <section ref={captureRef} className={styled.seatGame}>
       <section>
         <div className={styled.seatInfo}>자리선정순서</div>
-        <section className={styled.seatTable}></section>
+        <section className={styled.seatTable}>
+          {seatList.map((student, index) => (
+            <div className={styled.mySeat}>
+              <span className={styled.order}>{index + 1}</span>
+              <span>{student !== null ? student : ""}</span>
+            </div>
+          ))}
+        </section>
         <button onClick={() => navigate("/game/")}>
           <img className={styled.back} src={Back} alt="뒤로가자" />
         </button>
@@ -77,6 +146,9 @@ export default function SeatGame() {
           <div>{studentName}</div>
         </div>
       </section>
+      <button className={styled.downloadBtn} onClick={handleCapture}>
+        결과 다운로드
+      </button>
     </section>
   );
 }
