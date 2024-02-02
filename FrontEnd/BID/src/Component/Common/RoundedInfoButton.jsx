58c1c335015193c935aa76165559b334b6df5c42
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-02-02 17:55:56 +0900

    [S10P12A306-392] design: Modify button design [S10P12A306-392] rename: Move to "Common"

--- FrontEnd/BID/src/Component/Common/RoundedInfoButton.jsx
+++ FrontEnd/BID/src/Component/Common/RoundedInfoButton.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import styled from "./ProductInfoButton.module.css"
+import styled from "./RoundedInfoButton.module.css"
 
-export default function ProductInfoButton(props){
-	const { value, unit, textColor, borderColor, backgroundColor } = props;
+export default function RoundedInfoButton(props){
+	const { value, unit, textColor, borderColor, backgroundColor, padding } = props;
   const text = value +  unit;
   const wrapperStyle = {
     'border': '0.3vh solid ' + borderColor,
-    'backgroundColor': backgroundColor
+    'backgroundColor': backgroundColor,
+    'padding': padding,
   }
   const textStyle = {
     'color': textColor,
