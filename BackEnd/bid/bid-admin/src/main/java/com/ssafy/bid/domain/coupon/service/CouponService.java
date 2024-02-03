c33769c67e75cb530202afdb7065e8bc7e1af705
olrlobt <317tmdgjs@naver.com>
2024-02-03 23:20:18 +0900

    [S10P12A306-245] fix: Add missing code from merge

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/service/CouponService.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/service/CouponService.java
@@ -1,18 +1,18 @@
 package com.ssafy.bid.domain.coupon.service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ssafy.bid.domain.coupon.Coupon;
 import com.ssafy.bid.domain.coupon.CouponStatus;
+import com.ssafy.bid.domain.coupon.UserCoupon;
 import com.ssafy.bid.domain.coupon.dto.CouponListResponse;
 import com.ssafy.bid.domain.coupon.dto.CouponResponse;
+import com.ssafy.bid.domain.coupon.dto.UserCouponResponse;
 import com.ssafy.bid.domain.coupon.repository.CouponRepository;
+import com.ssafy.bid.domain.coupon.repository.UserCouponRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -23,10 +23,10 @@ import lombok.RequiredArgsConstructor;
 public class CouponService {
 
 	private final CouponRepository couponRepository;
+	private final UserCouponRepository userCouponRepository;
 
 	@Transactional(readOnly = true)
 	public CouponListResponse findCoupons(int gradeNo) {
-
 		List<CouponResponse> registeredCoupons = couponRepository
 			.findByGradeNoAndCouponStatus(gradeNo, CouponStatus.REGISTERED)
 			.stream()
@@ -52,18 +52,29 @@ public class CouponService {
 		return new CouponListResponse(registeredCoupons, unregisteredCoupons);
 	}
 
-	public void acceptCoupon(int couponNo) {
-		Coupon coupon = couponRepository.findById(couponNo)
-			.orElseThrow(() -> new NoSuchElementException("쿠폰이 없습니다."));
-
-		coupon.accept();
+	@Transactional(readOnly = true)
+	public List<UserCouponResponse> findCouponRequests(int gradeNo) {
+		return userCouponRepository.findUserCouponList(gradeNo);
 	}
 
-	public void deleteCoupon(int couponNo) {
-		if (!couponRepository.existsById(couponNo)) {
+	public void acceptCouponRequest(long userCouponNo) {
+		if (!userCouponRepository.existsById(userCouponNo)) {
 			throw new EntityNotFoundException("쿠폰이 없습니다.");
 		}
-		couponRepository.deleteById(couponNo);
+		// Todo. 해당 학생 알림 주기
+		userCouponRepository.deleteById(userCouponNo);
 	}
+
+	public void rejectCouponRequest(long userCouponNo) {
+		if (!userCouponRepository.existsById(userCouponNo)) {
+			throw new EntityNotFoundException("쿠폰이 없습니다.");
+		}
+
+		UserCoupon userCoupon = userCouponRepository.findById(userCouponNo)
+			.orElseThrow(() -> new NoSuchElementException("사용할 쿠폰이 없습니다."));
+
+		userCoupon.reject();
+	}
+
 
 }
