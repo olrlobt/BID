4cf9ee43e30027fc7d0a8d783a5a27a9bfe5c9f9
olrlobt <317tmdgjs@naver.com>
2024-02-03 23:10:03 +0900

    [S10P12A306-226] fix: Modify admin findCoupons response type

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/service/CouponService.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/service/CouponService.java
@@ -3,6 +3,7 @@ package com.ssafy.bid.domain.coupon.service;
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ssafy.bid.domain.coupon.Coupon;
 import com.ssafy.bid.domain.coupon.CouponStatus;
 import com.ssafy.bid.domain.coupon.dto.CouponListResponse;
+import com.ssafy.bid.domain.coupon.dto.CouponResponse;
 import com.ssafy.bid.domain.coupon.repository.CouponRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -25,11 +27,27 @@ public class CouponService {
 	@Transactional(readOnly = true)
 	public CouponListResponse findCoupons(int gradeNo) {
 
-		List<Coupon> registeredCoupons = couponRepository
-			.findByGradeNoAndCouponStatus(gradeNo, CouponStatus.REGISTERED);
+		List<CouponResponse> registeredCoupons = couponRepository
+			.findByGradeNoAndCouponStatus(gradeNo, CouponStatus.REGISTERED)
+			.stream()
+			.map(coupon -> CouponResponse.builder()
+				.no(coupon.getNo())
+				.name(coupon.getName())
+				.description(coupon.getDescription())
+				.gradeNo(coupon.getGradeNo())
+				.build()
+			).toList();
 
-		List<Coupon> unregisteredCoupons = couponRepository
-			.findByGradeNoAndCouponStatus(gradeNo, CouponStatus.UNREGISTERED);
+		List<CouponResponse> unregisteredCoupons = couponRepository
+			.findByGradeNoAndCouponStatus(gradeNo, CouponStatus.UNREGISTERED)
+			.stream()
+			.map(coupon -> CouponResponse.builder()
+				.no(coupon.getNo())
+				.name(coupon.getName())
+				.description(coupon.getDescription())
+				.gradeNo(coupon.getGradeNo())
+				.build()
+			).toList();
 
 		return new CouponListResponse(registeredCoupons, unregisteredCoupons);
 	}
