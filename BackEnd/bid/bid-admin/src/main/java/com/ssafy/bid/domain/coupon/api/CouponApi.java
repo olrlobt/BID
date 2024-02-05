269bbe73483b307895f44bb561c63c30e15b9e7a
olrlobt <317tmdgjs@naver.com>
2024-02-05 19:10:15 +0900

    [S10P12A306-674] feat: Add admin addCoupon api

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/api/CouponApi.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/api/CouponApi.java
@@ -2,14 +2,18 @@ package com.ssafy.bid.domain.coupon.api;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.bid.domain.coupon.dto.CouponCreateRequest;
 import com.ssafy.bid.domain.coupon.dto.CouponListResponse;
 import com.ssafy.bid.domain.coupon.dto.UserCouponResponse;
 import com.ssafy.bid.domain.coupon.service.CouponService;
@@ -28,6 +32,13 @@ public class CouponApi {
 	@GetMapping("/{gradeNo}/coupons")
 	public ResponseEntity<CouponListResponse> findCoupons(@PathVariable int gradeNo) {
 		return ResponseEntity.ok(couponService.findCoupons(gradeNo));
+	}
+
+	@PostMapping("/{gradeNo}/coupons")
+	public ResponseEntity<?> addCoupon(@PathVariable int gradeNo,
+		@RequestBody CouponCreateRequest couponCreateRequest) {
+		couponService.addCoupon(gradeNo, couponCreateRequest);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
 	@GetMapping("/{gradeNo}/user-coupons")
