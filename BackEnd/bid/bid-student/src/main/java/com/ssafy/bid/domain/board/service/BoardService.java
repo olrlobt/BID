27173081e5283727303bbf2683e4744e646d7cb1
olrlobt <317tmdgjs@naver.com>
2024-02-05 19:47:19 +0900

    [S10P12A306-483] feat: Add student addBoard api

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/board/service/BoardService.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/board/service/BoardService.java
@@ -3,8 +3,11 @@ package com.ssafy.bid.domain.board.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bid.domain.board.Board;
 import com.ssafy.bid.domain.board.Category;
+import com.ssafy.bid.domain.board.dto.BoardCreateRequest;
 import com.ssafy.bid.domain.board.dto.BoardResponse;
 import com.ssafy.bid.domain.board.dto.MyBoardsResponse;
 import com.ssafy.bid.domain.board.repository.BoardRepository;
@@ -15,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardService {
 
 	private final BoardRepository boardRepository;
@@ -29,5 +33,11 @@ public class BoardService {
 		List<BoardResponse> myBiddingBoards = boardRepository.findMyBiddingBoards(userNo);
 
 		return new MyBoardsResponse(myBoards, myBiddingBoards);
+	}
+
+	@Transactional
+	public void addBoard(int userNo, int gradeNo, BoardCreateRequest boardCreateRequest) {
+		Board board = boardCreateRequest.toEntity(1, 1);
+		boardRepository.save(board);
 	}
 }
