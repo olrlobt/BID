823b3e7814d31a5af374c36d6a77c48ee5ad9a6f
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-06 09:25:42 +0900

    [S10P12A306-683] refactor: Reformat codes

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/user/api/UserApi.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/user/api/UserApi.java
@@ -1,60 +1,61 @@
 package com.ssafy.bid.domain.user.api;
 
-import com.ssafy.bid.domain.user.dto.*;
-import com.ssafy.bid.domain.user.service.AuthService;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.bid.domain.user.dto.AccountRequest;
 import com.ssafy.bid.domain.user.dto.AccountResponse;
 import com.ssafy.bid.domain.user.dto.BallsResponse;
+import com.ssafy.bid.domain.user.dto.LoginRequest;
 import com.ssafy.bid.domain.user.dto.StudentRequest;
 import com.ssafy.bid.domain.user.dto.StudentResponse;
 import com.ssafy.bid.domain.user.dto.StudentsResponse;
+import com.ssafy.bid.domain.user.service.AuthService;
 import com.ssafy.bid.domain.user.service.UserService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins = "*")
 public class UserApi {
 
-    private final UserService userService;
-    private final AuthService authService;
+	private final UserService userService;
+	private final AuthService authService;
 
-    @GetMapping("/{gradeNo}/users")
-    public List<StudentsResponse> findStudents(@PathVariable int gradeNo) {
-        return userService.findStudents(gradeNo);
-    }
+	@GetMapping("/{gradeNo}/users")
+	public List<StudentsResponse> findStudents(@PathVariable int gradeNo) {
+		return userService.findStudents(gradeNo);
+	}
 
-    @GetMapping("/users/{userNo}")
-    public StudentResponse findStudent(@PathVariable int userNo, @ModelAttribute StudentRequest studentRequest) {
-        return userService.findStudent(userNo, studentRequest);
-    }
+	@GetMapping("/users/{userNo}")
+	public StudentResponse findStudent(@PathVariable int userNo, @ModelAttribute StudentRequest studentRequest) {
+		return userService.findStudent(userNo, studentRequest);
+	}
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
-        String token = this.authService.login(request);
-        return ResponseEntity.status(HttpStatus.OK).body(token);
-    }
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
+		String token = this.authService.login(request);
+		return ResponseEntity.status(HttpStatus.OK).body(token);
+	}
 
-    @PostMapping("/logout")
-    public ResponseEntity<?> logout(@RequestHeader("Authorization") String authToken) {
-        authService.logout(authToken);
-        return ResponseEntity.ok().build();
-    }
+	@PostMapping("/logout")
+	public ResponseEntity<?> logout(@RequestHeader("Authorization") String authToken) {
+		authService.logout(authToken);
+		return ResponseEntity.ok().build();
+	}
 
 	@GetMapping("/users/{userNo}/accounts")
 	public List<AccountResponse> findAccount(@PathVariable int userNo, @ModelAttribute AccountRequest accountRequest) {
--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/user/security/JwtProperties.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/user/security/JwtProperties.java
@@ -1,16 +1,17 @@
 package com.ssafy.bid.domain.user.security;
 
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
 
 @Getter
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    private final String secret;
-    private final long expirationTime;
+	private final String secret;
+	private final long expirationTime;
 
-    public JwtProperties(String secret, long expirationTime) {
-        this.secret = secret;
-        this.expirationTime = expirationTime;
-    }
+	public JwtProperties(String secret, long expirationTime) {
+		this.secret = secret;
+		this.expirationTime = expirationTime;
+	}
 }
