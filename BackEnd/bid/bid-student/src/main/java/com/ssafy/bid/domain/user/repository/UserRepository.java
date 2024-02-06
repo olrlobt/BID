6c3230c24ec101c1b22cb872a1a5bc351d16f64b
olrlobt <317tmdgjs@naver.com>
2024-02-06 20:57:07 +0900

    [S10P12A306-668] !HOTFIX: Add missing code

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/user/repository/UserRepository.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.bid.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.bid.domain.user.User;
