d768d88a3906b3cbf72dfde1a3efde4860331fea
백지윤 <wendy6992@naver.com>
2024-02-06 09:48:46 +0900

    [S10P12A306-747] feat: Add mypage

---
+++ FrontEnd/BID/src/Page/Student/MyPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import BarChart from "../../Component/FinData/BarChart";
+import PieChart from "../../Component/FinData/PieChart";
+import Coupon from "../../Component/FinData/Coupon";
+import AttendRec from '../../Component/FinData/AttendRec'
+import CalendarChart from "../../Component/FinData/CalendarChart";
+import styled from './MyPage.module.css';
+
+function MyPage() {
+
+    const categoryData = [
+        { category: '식비', percentage: 30 },
+        { category: '교통', percentage: 20 },
+        // 다른 카테고리들도 추가
+      ];
+
+      
+    return(
+        <div className={styled.section}>
+            <div className={styled.dashboardContainer}>
+                <div className={styled.chartsContainer}>
+                    <BarChart />
+                    <PieChart data={categoryData} />
+                    <Coupon />
+                </div>
+            <div className={styled.additionalChartsContainer}>
+                <AttendRec className={styled.AttendRec} />
+                <CalendarChart />
+            </div>
+        </div>    
+    </div>
+    ) 
+}
+
+export default MyPage;
+
+
