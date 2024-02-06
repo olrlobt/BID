2854586926c09754bd21c0b883610d12c2708d91
김예림 <kimyelim100@naver.com>
2024-02-06 14:42:46 +0900

    [S10P12A306-717] feat: Add Game Ball API Test

--- FrontEnd/BID/src/Page/Manage/GamePage.jsx
+++ FrontEnd/BID/src/Page/Manage/GamePage.jsx
@@ -1,106 +1,48 @@
 import { Link } from "react-router-dom";
 import styled from "./GamePage.module.css";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 import useBalls from "../../hooks/useBalls";
 import { ballSelector } from "../../Store/ballSlice";
+import { useQuery } from "@tanstack/react-query";
+import { viewStudentBalls } from "../../Apis/TeacherManageApis";
 
 export default function GamePage() {
-  // 벡엔으로부터 데이터 받아와~
-  const dummyBalls = [
-    {
-      no: 1,
-      name: "강하윤",
-      ballCount: 4,
-    },
-    {
-      no: 2,
-      name: "김성민",
-      ballCount: 3,
-    },
-    {
-      no: 3,
-      name: "박준영",
-      ballCount: 2,
-    },
-    {
-      no: 4,
-      name: "강지민",
-      ballCount: 1,
-    },
-    {
-      no: 5,
-      name: "김지우",
-      ballCount: 1,
-    },
-    {
-      no: 6,
-      name: "김서연",
-      ballCount: 5,
-    },
-    {
-      no: 7,
-      name: "김다은",
-      ballCount: 2,
-    },
-    {
-      no: 8,
-      name: "김서연",
-      ballCount: 5,
-    },
-    {
-      no: 9,
-      name: "김성민",
-      ballCount: 3,
-    },
-    {
-      no: 10,
-      name: "김민서",
-      ballCount: 2,
-    },
-    {
-      no: 11,
-      name: "김성민",
-      ballCount: 2,
-    },
-    {
-      no: 12,
-      name: "김지우",
-      ballCount: 1,
-    },
-  ];
-
   const { initBalls } = useBalls();
   const ballList = useSelector(ballSelector);
 
-  useEffect(() => {
-    // dummyBalls 백엔드 api 날려서 넣어줄 것
-    initBalls({ ballList: dummyBalls });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { data: stuBalls } = useQuery({
+    queryKey: ["studentBall"],
+    queryFn: () =>
+      viewStudentBalls().then((res) => {
+        initBalls({ ballList: res.data });
+        return res.data;
+      }),
+  });
 
   return (
     <>
-      <section className={styled.gamePage}>
-        <h1>이번 달 자리는 누구부터 정할까요?</h1>
-        <section className={styled.gameTable}>
-          <div className={styled.tableTitle}>
-            우리 반 친구들의 자리 구슬 개수
-          </div>
-          <section className={styled.ballStage}>
-            {ballList &&
-              ballList.map((student) => (
-                <div className={styled.studentList}>
-                  <span>{student.name}</span>
-                  <span>{student.ballCount}개</span>
-                </div>
-              ))}
+      {stuBalls && (
+        <section className={styled.gamePage}>
+          <h1>이번 달 자리는 누구부터 정할까요?</h1>
+          <section className={styled.gameTable}>
+            <div className={styled.tableTitle}>
+              우리 반 친구들의 자리 구슬 개수
+            </div>
+            <section className={styled.ballStage}>
+              {ballList &&
+                ballList.map((student) => (
+                  <div className={styled.studentList} key={ballList.no}>
+                    <span>{student.name}</span>
+                    <span>{student.ballCount}개</span>
+                  </div>
+                ))}
+            </section>
           </section>
+          <Link to="/game/seat" state={ballList}>
+            <button className={styled.seatBtn}>자리 뽑기 시작</button>
+          </Link>
         </section>
-        <Link to="/game/seat" state={ballList}>
-          <button className={styled.seatBtn}>자리 뽑기 시작</button>
-        </Link>
-      </section>
+      )}
     </>
   );
 }
