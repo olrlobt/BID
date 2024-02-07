faf145587f8aac85385332cc4e8acace42347bc3
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-08 00:12:18 +0900

    [S10P12A306-680] Refactor: Refactor rewards management sources

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/reward/service/RewardService.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/reward/service/RewardService.java
@@ -2,14 +2,14 @@ package com.ssafy.bid.domain.reward.service;
 
 import java.util.List;
 
+import com.ssafy.bid.domain.reward.dto.RewardListGetResponse;
 import com.ssafy.bid.domain.reward.dto.RewardSaveRequest;
 import com.ssafy.bid.domain.reward.dto.RewardSendRequest;
-import com.ssafy.bid.domain.reward.dto.RewardsFindResponse;
 
 public interface RewardService {
 	void saveReward(int gradeNo, RewardSaveRequest rewardSaveRequest);
 
-	List<RewardsFindResponse> findRewards(int gradeNo);
+	List<RewardListGetResponse> getRewards(int gradeNo);
 
 	void deleteReward(int rewardNo);
 
