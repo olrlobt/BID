443a86d046597b109429f508e787c2f1c828fb4f
olrlobt <317tmdgjs@naver.com>
2024-02-07 23:02:58 +0900

    [S10P12A306-222] feat: Add reply to boardDetail

---
+++ BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/domain/board/repository/CoreReplyCustomRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.ssafy.bid.domain.board.repository;
+
+import static com.ssafy.bid.domain.board.QReply.*;
+import static com.ssafy.bid.domain.user.QStudent.*;
+
+import java.util.List;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.bid.domain.board.dto.ReplyResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CoreReplyCustomRepositoryImpl implements CoreReplyCustomRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	public List<ReplyResponse> findReplies(long boardNo){
+
+		return queryFactory.select(Projections.constructor(ReplyResponse.class,
+				reply.content,
+				student.name
+			))
+			.from(reply)
+			.innerJoin(student)
+			.on(reply.boardNo.eq(boardNo).and(reply.userNo.eq(student.no)))
+			.fetch();
+	}
+
+}
