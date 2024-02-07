01334a889e0b907624d0037354c0ef65809895d4
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-08 00:57:40 +0900

    [S10P12A306-680] Refactor: Refactor savings management sources

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/saving/dto/SavingListUpdateRequest.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/saving/dto/SavingListUpdateRequest.java
@@ -1,12 +1,12 @@
 package com.ssafy.bid.domain.saving.dto;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class SavingRequest {
-	private int no;
-	private int depositPrice;
-	private int interestRate;
+public class SavingListUpdateRequest {
+	private List<SavingUpdateRequest> savingUpdateRequests;
 }
