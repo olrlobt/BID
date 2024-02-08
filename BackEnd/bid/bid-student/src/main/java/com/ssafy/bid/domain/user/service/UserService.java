e89324fdb299aac99adaf2e9375d225a2d641c2f
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-08 09:32:45 +0900

    [S10P12A306-680] refactor: Refactor users management sources

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/user/service/UserService.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/user/service/UserService.java
@@ -1,13 +1,10 @@
 package com.ssafy.bid.domain.user.service;
 
 import com.ssafy.bid.domain.user.dto.AttendanceResponse;
-import com.ssafy.bid.domain.user.dto.PasswordResetRequest;
 
 public interface UserService {
 
 	void checkAttendance(int userNo);
-
-	void resetPassword(String userId, PasswordResetRequest passwordResetRequest);
 
 	AttendanceResponse getStudentAttendance(Integer userNo);
 }
