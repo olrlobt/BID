d62b102d3e34eead057d0ca2483db245c141e9c9
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-08 09:08:55 +0900

    [S10P12A306-680] refactor: Refactor savings management sources

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/service/SavingService.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/saving/service/SavingService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import com.ssafy.bid.domain.saving.dto.SavingExpireAlertRequest;
 import com.ssafy.bid.domain.saving.dto.SavingSaveRequest;
 import com.ssafy.bid.domain.saving.dto.SavingTransferAlertRequest;
+import com.ssafy.bid.domain.saving.dto.UserSavingListGetResponse;
 import com.ssafy.bid.domain.user.dto.CustomUserInfo;
 
 public interface SavingService {
+	List<UserSavingListGetResponse> getAllSavings(int gradeNo, int userNo);
 
 	void saveSavings(CustomUserInfo userInfo, SavingSaveRequest savingSaveRequest);
 
