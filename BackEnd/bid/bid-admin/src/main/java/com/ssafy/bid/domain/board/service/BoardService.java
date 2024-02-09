9b225d6c57aae3e1ac058890a65476b6676cabe0
olrlobt <317tmdgjs@naver.com>
2024-02-09 15:15:43 +0900

    [S10P12A306-658] feat: Add validation checks in deleteBoard api

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/board/service/BoardService.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/board/service/BoardService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ssafy.bid.domain.board.dto.BoardListResponse;
 import com.ssafy.bid.domain.board.repository.BoardRepository;
 import com.ssafy.bid.domain.board.repository.ReplyRepository;
+import com.ssafy.bid.global.error.exception.ResourceNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,9 +29,12 @@ public class BoardService {
 	}
 
 	@Transactional
-	public void deleteBoard(long boardNo) {
+	public void deleteBoard(long boardNo, int gradeNo, int userNo) {
+
+		// user와 gradeNo이 다를 경우 error
+
 		if (!boardRepository.existsById(boardNo)) {
-			throw new NoSuchElementException("해당 게시글이 없습니다.");
+			throw new ResourceNotFoundException("해당 게시글이 없습니다.", boardNo);
 		}
 		boardRepository.deleteById(boardNo);
 	}
