ff8cdaca306175fe58ffbc593bc2952229ef4214
olrlobt <317tmdgjs@naver.com>
2024-02-09 16:22:47 +0900

    [S10P12A306-659] feat: Add validation checks in register&unRegisterCoupon api

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/repository/CouponRepository.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/repository/CouponRepository.java
@@ -1,11 +1,16 @@
 package com.ssafy.bid.domain.coupon.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.bid.domain.coupon.Coupon;
+import com.ssafy.bid.domain.coupon.CouponStatus;
+
+import jakarta.validation.constraints.NotNull;
 
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
 	List<Coupon> findByGradeNo(int gradeNo);
+	Optional<Coupon> findByNoAndCouponStatus(Integer no, @NotNull CouponStatus couponStatus);
 }
