65a96df129f6de170a652e357293039c81ec864e
olrlobt <317tmdgjs@naver.com>
2024-02-10 01:57:47 +0900

    [S10P12A306-476] refactor: Modify Exceptions

--- BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/global/error/exception/AuthenticationFailedException.java
+++ BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/global/error/exception/AuthenticationFailedException.java
@@ -1,10 +1,12 @@
 package com.ssafy.bid.global.error.exception;
 
-/**
- * 요청이 인증을 필요로 함.
- * ex) 로그인 실패
- */
+
 public class AuthenticationFailedException extends RuntimeException {
+
+	/**
+	 * 요청이 인증을 필요로 함.
+	 * ex) 로그인 실패
+	 */
 	public AuthenticationFailedException(String message) {
 		super(message);
 	}
--- BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/global/error/exception/MissingParameterException.java
+++ BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/global/error/exception/MissingParameterException.java
@@ -1,19 +1,21 @@
 package com.ssafy.bid.global.error.exception;
 
+import java.util.Arrays;
+
 import lombok.Getter;
 
-/**
- * 필수 파라미터가 요청에 누락됨.
- */
+
 
 @Getter
 public class MissingParameterException extends RuntimeException {
 
-	private final Object parameterValue;
+	private final Object[] parameterValues;
 
-	public MissingParameterException(String message, Object parameterValue) {
-		super(message + " Parameter Value: " + parameterValue.toString());
-		this.parameterValue = parameterValue;
-
+	/**
+	 * 필수 파라미터가 요청에 누락됨.
+	 */
+	public MissingParameterException(String message, Object... parameterValues) {
+		super(message + " Parameter Value: " + Arrays.toString(parameterValues));
+		this.parameterValues = parameterValues;
 	}
 }
