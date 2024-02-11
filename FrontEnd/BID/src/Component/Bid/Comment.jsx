bf86b8ca4ea953fe043c6f6652407ca86e67b91e
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-02-11 14:55:26 +0900

    fix: Fix ESLint

--- FrontEnd/BID/src/Component/Bid/Comment.jsx
+++ FrontEnd/BID/src/Component/Bid/Comment.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "./Comment.module.css";
-import { Delete } from "@material-ui/icons";
-import SettingButton from "../Common/SettingButton"
 import { useMutation } from "@tanstack/react-query";
 import { deleteCommentApi } from "../../Apis/TeacherBidApis";
 
@@ -45,7 +43,7 @@ export default function Comment(props){
     <div className={styled.commentWrapper} style={isWriter? writerLayout: null}>
       <div className={styled.left}>
         <div className={styled.profile}>
-          {/* <img src={ userImgUrl } alt="" /> */}
+          <img src={ userImgUrl } alt="" />
         </div>
       </div>
       <div className={styled.right} style={isWriter? writerBox: null}>
