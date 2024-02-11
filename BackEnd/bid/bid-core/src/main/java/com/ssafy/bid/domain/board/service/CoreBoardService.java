156becd65a696de2dda336fff2f4467ec1ca4c59
olrlobt <317tmdgjs@naver.com>
2024-02-12 07:47:19 +0900

    [S10P12A306-773] feat: Add holdBoards api

--- BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/domain/board/service/CoreBoardService.java
+++ BackEnd/bid/bid-core/src/main/java/com/ssafy/bid/domain/board/service/CoreBoardService.java
@@ -73,7 +73,7 @@ public class CoreBoardService {
 		Instant instant = dateTime.atZone(ZoneId.systemDefault()).toInstant();
 
 		taskScheduler.schedule(() -> {
-			coreBoardScheduleService.job(boardNo);
+			coreBoardScheduleService.bidProgress(boardNo);
 		}, instant);
 	}
 
