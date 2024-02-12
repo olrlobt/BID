0138e0f12bcc6090994257784ac95e37aeca686b
백지윤 <wendy6992@naver.com>
2024-02-12 15:25:21 +0900

    [S10P12A306-758] feat: Add verifylogic

--- FrontEnd/BID/src/Page/User/RegisterPage.jsx
+++ FrontEnd/BID/src/Page/User/RegisterPage.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "./RegisterPage.module.css";
 import { Link } from "react-router-dom";
 import useModal from "../../hooks/useModal";
+import { useSelector } from "react-redux";
 import Logo from "../../Component/Common/Logo";
-import { addUserApi, duplicateIdApi } from "../../Apis/UserApis";
+import { addUserApi, duplicateIdApi, registerCodeApi } from "../../Apis/UserApis";
 import { useMutation } from "@tanstack/react-query";
+import { SvgIcon } from "@material-ui/core";
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import SchoolIcon from '@mui/icons-material/School';
 
 function RegisterPage() {
   const [id, setId] = useState("");
@@ -15,15 +21,35 @@ function RegisterPage() {
   const [verifyNo, setVerifyNo] = useState("");
   const [schoolNo, setSchoolNo] = useState("");
   const { openModal } = useModal();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   /** 아이디 중복 검사 */
   const duplicateIdQuery = useMutation({
-    mutationKey: ["sendCode"],
+    mutationKey: ["duplicateId"],
     mutationFn: (userData) => duplicateIdApi(userData),
-    onSuccess: (data) => {
-      console.log(data);
-      // Move to the next step upon successful submission
-      // setStep(2);
+    onSuccess: (res) => {
+      console.log(res);
+      if (res.data === true) {
+        setErrorMessage("이미 존재하는 아이디입니다.");
+      } else {
+        setSuccessMessage("사용 가능한 아이디입니다.");
+      }
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  /** 전화번호 코드 전송 */
+  const registerCodeQuery = useMutation({
+    mutationKey: ["sendCode"],
+    mutationFn: (userTel) => registerCodeApi(userTel),
+    onSuccess: (res) => {
+      console.log(res);
+      if (res.data === true) {
+      } else {
+      }
     },
     onError: (error) => {
       console.log(error);
@@ -57,15 +83,40 @@ function RegisterPage() {
     addUserQuery.mutate(userData);
   };
 
-  const handleDuplicateId = (e) => {
-    // 코드 발송 동작 구현
-    e.preventDefault();
-    let userData = {
-      id,
-    };
-    duplicateIdQuery.mutate(userData);
-  };
   
+  const handleDuplicateId = (e) => {
+    e.preventDefault();
+    if (!id) {
+      setErrorMessage("아이디를 입력하세요.");
+    } else {
+      setErrorMessage(""); // 새로운 검색을 시작하기 전에 기존 메시지 초기화
+      let userData = id;
+      duplicateIdQuery.mutate(userData);
+    }
+  };
+
+  const handleSendCodeBtn = (e) => {
+    e.preventDefault()
+    if (!tel) {
+      setErrorMessage("휴대전화 번호를 입력하세요.")
+    }
+    else {
+      setErrorMessage("")
+      let userTel = {
+        tel
+      }
+      registerCodeQuery.mutate(userTel);
+    }
+  }
+
+  const handlerModalClose = (schoolNo) => {
+    console.log(schoolNo)
+    if (schoolNo) {
+      setSchoolNo(schoolNo)
+    }
+  }
+
+
 
   return (
     <section className={styled.back}>
@@ -74,37 +125,73 @@ function RegisterPage() {
       </div>
       <div className={styled.content}>
         <form className={styled.contentInput}>
+        {errorMessage && <p className={styled.errorMessage}>{errorMessage}</p>}
+        {successMessage && <p className={styled.successMessage}>{successMessage}</p>}
         <div className={styled.registerInput}>
-          <input
-            type="text"
-            placeholder="아이디"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
+          <div className={styled.inputWithIcon}>
+            <SvgIcon 
+            component={PersonIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
+            <input
+              type="text"
+              placeholder="아이디"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+            </div>
           <button
               className={styled.duplicateIdBtn}
               onClick={handleDuplicateId}
           >
           중복 검사
           </button>
+          <div className={styled.inputWithIcon}>
+          <SvgIcon 
+            component={LockIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
           <input
             type="password"
             placeholder="비밀번호"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          </div>
+          <div className={styled.inputWithIcon}>
+          <SvgIcon 
+            component={LockIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
           <input
             type="password"
             placeholder="비밀번호 확인"
             value={confirmPassword}
             onChange={(e) => setconfirmPassword(e.target.value)}
           />
+          </div>
+          <div className={styled.inputWithIcon}>
+          <SvgIcon 
+            component={PersonIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
           <input
             type="text"
             placeholder="이름"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          </div>
+          <div className={styled.inputWithIcon}>
+          <SvgIcon 
+            component={PhoneIphoneIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
           <input
             type="text"
             placeholder="휴대전화번호"
@@ -113,21 +200,34 @@ function RegisterPage() {
           />
           <button
               className={styled.sendCodeBtn}
-              // onClick={handleSendCodeBtn}
+              onClick={handleSendCodeBtn}
           >
           코드 발송
           </button>
+          </div>
+          <div className={styled.inputWithIcon}>
+          <SvgIcon 
+            component={PhoneIphoneIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
           <input
             type="text"
             placeholder="인증코드"
             value={verifyNo}
             onChange={(e) => setVerifyNo(e.target.value)}
           />
+          </div>
+          <div className={styled.inputWithIcon}>
+          <SvgIcon 
+            component={SchoolIcon} 
+            style={{ fill: "#4D4D4D", height: "2.5vh" }}
+            className={styled.icon}
+            />
           <input
             type="text"
             placeholder="학교코드"
-            value={schoolNo}
-            onChange={(e) => setSchoolNo(e.target.value)}
+            value={schoolNo} // Display selected school code
           />
           <button
             className={styled.searchSchool}
@@ -135,18 +235,19 @@ function RegisterPage() {
               e.preventDefault(); // Prevent default form submission behavior
               openModal({
                 type: "searchSchool",
-                props: ["학교 검색"]
+                props: ["학교 검색", handlerModalClose]
               });
             }}
           >
-          학교 검색
+            학교 검색
           </button>
+          </div>
           <Link
             to="/login"
             className={styled.joinLink}
             onClick={handleRegisterEvent}
           >
-            <p>JOIN</p>
+            JOIN
           </Link>
           </div>
         </form>
