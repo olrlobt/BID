259b6bcceefd972b8e1af6648b566084257b9237
olrlobt <317tmdgjs@naver.com>
2024-02-12 15:04:39 +0900

    [S10P12A306-773] feat: Add Hold NotificationType

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/grade/repository/StudentRepository.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/grade/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.bid.domain.grade.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.bid.domain.user.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 	void deleteAllByGradeNo(int gradeNo);
+	List<Student> findAllByGradeNo(Integer gradeNo);
 }
