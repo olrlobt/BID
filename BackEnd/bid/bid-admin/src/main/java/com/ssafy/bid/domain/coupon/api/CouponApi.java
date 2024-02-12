b3251c8a016c879dbded6272cdcf06aad876ec3e
olrlobt <317tmdgjs@naver.com>
2024-02-12 20:45:47 +0900

    [S10P12A306-657, S10P12A306-660] feat: Add user validation

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/api/CouponApi.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/coupon/api/CouponApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import com.ssafy.bid.domain.coupon.dto.CouponCreateRequest;
 import com.ssafy.bid.domain.coupon.dto.CouponListResponse;
 import com.ssafy.bid.domain.coupon.dto.UserCouponResponse;
 import com.ssafy.bid.domain.coupon.service.CouponService;
+import com.ssafy.bid.domain.user.service.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,50 +32,66 @@ public class CouponApi {
 	private final CouponService couponService;
 
 	@GetMapping("/{gradeNo}/coupons")
-	public ResponseEntity<CouponListResponse> findCoupons(@PathVariable int gradeNo) {
-		return ResponseEntity.ok(couponService.findCoupons(gradeNo));
+	public ResponseEntity<CouponListResponse> findCoupons(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo) {
+		int userNo = userDetails.getUserInfo().getNo();
+		return ResponseEntity.ok(couponService.findCoupons(gradeNo, userNo));
 	}
 
 	@PostMapping("/{gradeNo}/coupons")
-	public ResponseEntity<?> addCoupon(@PathVariable int gradeNo,
+	public ResponseEntity<?> addCoupon(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo,
 		@RequestBody CouponCreateRequest couponCreateRequest) {
-		couponService.addCoupon(gradeNo, couponCreateRequest);
+		int userNo = userDetails.getUserInfo().getNo();
+		couponService.addCoupon(gradeNo, couponCreateRequest, userNo);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
 	@DeleteMapping("/{gradeNo}/coupons/{couponNo}")
-	public ResponseEntity<?> deleteCoupon(@PathVariable int gradeNo,
+	public ResponseEntity<?> deleteCoupon(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo,
 		@PathVariable int couponNo) {
-		couponService.deleteCoupon(couponNo, gradeNo);
+		int userNo = userDetails.getUserInfo().getNo();
+		couponService.deleteCoupon(couponNo, gradeNo, userNo);
 		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping("/{gradeNo}/user-coupons")
-	public ResponseEntity<List<UserCouponResponse>> findUserCoupons(@PathVariable int gradeNo) {
-		return ResponseEntity.ok(couponService.findUserCoupons(gradeNo));
+	public ResponseEntity<List<UserCouponResponse>> findUserCoupons(
+		@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable int gradeNo) {
+		int userNo = userDetails.getUserInfo().getNo();
+		return ResponseEntity.ok(couponService.findUserCoupons(gradeNo, userNo));
 	}
 
 	@DeleteMapping("/{gradeNo}/user-coupons/{userCouponNo}")
-	public ResponseEntity<?> acceptUserCoupon(@PathVariable int gradeNo, @PathVariable int userCouponNo) {
-		couponService.acceptUserCoupon(userCouponNo);
+	public ResponseEntity<?> acceptUserCoupon(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo, @PathVariable int userCouponNo) {
+		int userNo = userDetails.getUserInfo().getNo();
+		couponService.acceptUserCoupon(userCouponNo, gradeNo, userNo);
 		return ResponseEntity.noContent().build();
 	}
 
 	@PatchMapping("/{gradeNo}/user-coupons/{userCouponNo}")
-	public ResponseEntity<?> rejectUserCoupon(@PathVariable int gradeNo, @PathVariable int userCouponNo) {
-		couponService.rejectUserCoupon(userCouponNo);
+	public ResponseEntity<?> rejectUserCoupon(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo, @PathVariable int userCouponNo) {
+		int userNo = userDetails.getUserInfo().getNo();
+		couponService.rejectUserCoupon(userCouponNo, gradeNo, userNo);
 		return ResponseEntity.noContent().build();
 	}
 
 	@PatchMapping("/{gradeNo}/coupons/{couponNo}/register")
-	public ResponseEntity<?> registerCoupon(@PathVariable int gradeNo, @PathVariable int couponNo) {
-		couponService.registerCoupon(couponNo);
+	public ResponseEntity<?> registerCoupon(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo, @PathVariable int couponNo) {
+		int userNo = userDetails.getUserInfo().getNo();
+		couponService.registerCoupon(couponNo, gradeNo, userNo);
 		return ResponseEntity.noContent().build();
 	}
 
 	@PatchMapping("/{gradeNo}/coupons/{couponNo}/unregister")
-	public ResponseEntity<?> unRegisterCoupon(@PathVariable int gradeNo, @PathVariable int couponNo) {
-		couponService.unRegisterCoupon(couponNo);
+	public ResponseEntity<?> unRegisterCoupon(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable int gradeNo, @PathVariable int couponNo) {
+		int userNo = userDetails.getUserInfo().getNo();
+		couponService.unRegisterCoupon(couponNo, gradeNo, userNo);
 		return ResponseEntity.noContent().build();
 	}
 }
