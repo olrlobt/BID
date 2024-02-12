7e84c42f4ca81cef4781e558a2a73f24af43e8a1
olrlobt <317tmdgjs@naver.com>
2024-02-12 21:43:14 +0900

    [S10P12A306-774] feat: Add automatic schedule of create new grade

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/gradeperiod/service/GradePeriodScheduler.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/gradeperiod/service/GradePeriodScheduler.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.bid.domain.grade.service.GradeService;
+import com.ssafy.bid.domain.board.service.BoardService;
 import com.ssafy.bid.domain.gradeperiod.GradePeriod;
 import com.ssafy.bid.global.util.CronExpression;
 
@@ -22,7 +22,7 @@ public class GradePeriodScheduler {
 
 	private final TaskScheduler taskScheduler;
 	private final Map<Integer, Map<Integer, ScheduledFuture<?>>> gradeScheduledTasks = new ConcurrentHashMap<>();
-	private final GradeService gradeService;
+	private final BoardService boardService;
 
 	public void scheduleClassLessonTask(GradePeriod gradePeriod) {
 		String cronExpressionOfStart = CronExpression.of(gradePeriod.getStartPeriod());
@@ -36,11 +36,11 @@ public class GradePeriodScheduler {
 		cancelScheduledTask(gradePeriod.getGradeNo(), gradePeriod.getSequence() * 2);
 
 		gradePeriodScheduled.put((gradePeriod.getSequence() * 2 - 1), taskScheduler.schedule(() -> {
-			gradeService.holdBid(gradePeriod.getGradeNo());
+			boardService.holdBid(gradePeriod.getGradeNo());
 		}, new CronTrigger(cronExpressionOfStart)));
 
 		gradePeriodScheduled.put(gradePeriod.getSequence() * 2, taskScheduler.schedule(() -> {
-			gradeService.unHoldBid(gradePeriod.getGradeNo());
+			boardService.unHoldBid(gradePeriod.getGradeNo());
 		}, new CronTrigger(cronExpressionOfEnd)));
 		gradeScheduledTasks.put(gradePeriod.getGradeNo(), gradePeriodScheduled);
 	}
