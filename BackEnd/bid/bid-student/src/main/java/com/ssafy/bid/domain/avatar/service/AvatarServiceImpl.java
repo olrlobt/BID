950d8af1ded7f96c74a09ca2af1fa426a2cb642f
olrlobt <317tmdgjs@naver.com>
2024-02-13 01:00:31 +0900

    [S10P12A306-781] feat: Add buyAvatar api

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/avatar/service/AvatarServiceImpl.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/avatar/service/AvatarServiceImpl.java
@@ -1,16 +1,23 @@
 package com.ssafy.bid.domain.avatar.service;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bid.domain.avatar.Avatar;
+import com.ssafy.bid.domain.avatar.UserAvatar;
+import com.ssafy.bid.domain.avatar.dto.AvatarRequest;
+import com.ssafy.bid.domain.avatar.dto.AvatarResponse;
 import com.ssafy.bid.domain.avatar.dto.UserAvatarUpdateRequest;
 import com.ssafy.bid.domain.avatar.dto.UserAvatarsGetResponse;
+import com.ssafy.bid.domain.avatar.repository.AvatarRepository;
 import com.ssafy.bid.domain.avatar.repository.UserAvatarRepository;
 import com.ssafy.bid.domain.user.Student;
 import com.ssafy.bid.domain.user.UserType;
 import com.ssafy.bid.domain.user.dto.CustomUserInfo;
+import com.ssafy.bid.domain.user.repository.StudentRepository;
 import com.ssafy.bid.domain.user.repository.UserRepository;
 import com.ssafy.bid.global.error.exception.AuthorizationFailedException;
 import com.ssafy.bid.global.error.exception.ResourceNotFoundException;
@@ -24,6 +31,7 @@ public class AvatarServiceImpl implements AvatarService {
 
 	private final UserAvatarRepository userAvatarRepository;
 	private final UserRepository userRepository;
+	private final AvatarRepository avatarRepository;
 
 	@Override
 	public List<UserAvatarsGetResponse> getUserAvatars(UserType userType, int userNo) {
@@ -49,5 +57,24 @@ public class AvatarServiceImpl implements AvatarService {
 				() -> new ResourceNotFoundException("아바타수정: UserAvatar 가 없음.", userAvatarUpdateRequest.getNo()));
 
 		student.updateAvatar(url);
+	}
+
+	@Override
+	@Transactional
+	public AvatarResponse buyAvatar(int userNo, AvatarRequest avatarRequest) {
+
+		Student student = userRepository.findStudentByUserNo(userNo)
+			.orElseThrow(() -> new ResourceNotFoundException("아바타구매: Student 가 없음.", userNo));
+
+		List<Avatar> allAvatars = avatarRepository.findAll();
+		Avatar avatar = allAvatars.get(ThreadLocalRandom.current().nextInt(allAvatars.size()));
+
+		userAvatarRepository.save(UserAvatar.builder()
+			.avatarNo(avatar.getNo())
+			.userNo(userNo)
+			.build());
+
+		student.subtractPrice(avatar.getPrice());
+		return avatar.toDto();
 	}
 }
--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/board/service/BoardService.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/board/service/BoardService.java
@@ -175,5 +175,4 @@ public class BoardService {
 			return HttpStatus.CREATED;
 		});
 	}
-
 }
