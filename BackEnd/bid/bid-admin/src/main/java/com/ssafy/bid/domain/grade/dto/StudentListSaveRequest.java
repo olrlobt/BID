1459741012ac1f7711568498d5e8fa08b2bb65e9
서울_17반_이현진 <hgene0929@gmail.com>
2024-02-13 20:47:29 +0900

    [S10P12A306-679] !HOTFIX

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/grade/dto/StudentListSaveRequest.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/grade/dto/StudentListSaveRequest.java
@@ -10,26 +10,39 @@ import lombok.Getter;
 
 @Getter
 public class StudentListSaveRequest {
-	private String id;
 	private String password;
 	private String name;
 	private String birthDate;
 	private int number;
 
 	public Student toEntity(PasswordEncoder passwordEncoder, int schoolNo, int gradeNo, String id) {
-		return Student.builder()
-			.id(id + String.format("%02d", this.number))
-			.password(passwordEncoder.encode(password))
-			.name(name)
-			.schoolNo(schoolNo)
-			.birthDate(birthDate)
-			.asset(0)
-			.ballCount(1)
-			.profileImgUrl("https://ssafya306.s3.ap-northeast-2.amazonaws.com/DefaultBody.png")
-			.attendance(new Attendance())
-			.expenditureStatistics(new ExpenditureStatistics())
-			.gradeNo(gradeNo)
-			.taxRate(3)
-			.build();
+		// return Student.builder()
+		// 	.id(id + String.format("%02d", this.number))
+		// 	.password(passwordEncoder.encode(password))
+		// 	.name(name)
+		// 	.schoolNo(schoolNo)
+		// 	.birthDate(birthDate)
+		// 	.asset(0)
+		// 	.ballCount(1)
+		// 	.profileImgUrl(imgUrl)
+		// 	.attendance(new Attendance())
+		// 	.expenditureStatistics(new ExpenditureStatistics())
+		// 	.gradeNo(gradeNo)
+		// 	.taxRate(3)
+		// 	.build();
+		return new Student(
+			id + String.format("%02d", this.number),
+			passwordEncoder.encode(password),
+			name,
+			schoolNo,
+			birthDate,
+			0,
+			1,
+			"https://ssafya306.s3.ap-northeast-2.amazonaws.com/DefaultBody.png",
+			new Attendance(),
+			new ExpenditureStatistics(),
+			gradeNo,
+			3
+		);
 	}
 }
