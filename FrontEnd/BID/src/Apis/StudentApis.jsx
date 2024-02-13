04682c297d148b3bfb7a53d3b7883948bea2c80b
김예림 <kimyelim100@naver.com>
2024-02-13 09:40:55 +0900

    fix: studentApis
    Former-commit-id: 6cb0cd297a3fcb67acb21e3340d7ccbdcf85a738

--- FrontEnd/BID/src/Apis/StudentApis.jsx
+++ FrontEnd/BID/src/Apis/StudentApis.jsx
@@ -1,7 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const StudentApis = axios.create({
   baseURL: process.env.REACT_APP_STU_API,
+});
+
+export const StudentRewardsApis = axios.create({
+  baseURL: process.env.REACT_APP_TCH_API,
 });
 
 /**
@@ -18,7 +22,7 @@ export const getStudentListApi = async () => {
  * @returns 적금 가입 내역
  */
 export const getStudentSavingInfo = async () => {
-  return await StudentApis.get('/savings');
+  return await StudentApis.get("/savings");
 };
 
 /**
@@ -28,5 +32,5 @@ export const getStudentSavingInfo = async () => {
  */
 
 export const applyStudentSaving = async (savingInfo) => {
-  return await StudentApis.post('/savings', savingInfo);
+  return await StudentApis.post("/savings", savingInfo);
 };
