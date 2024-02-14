8c14d0b85a72d4143e67d8c10a393d1aa687019e
김예림 <kimyelim100@naver.com>
2024-02-15 02:03:58 +0900

    [S10P12A306-807] feat: Add Income Info for student  UI

--- FrontEnd/BID/src/Page/Student/StudentSaving.jsx
+++ FrontEnd/BID/src/Page/Student/StudentSaving.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from '../Manage/BankPage.module.css';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
-import { savingSelector } from '../../Store/savingSlice';
 import {
   applyStudentSaving,
   getStudentSavingInfo,
@@ -15,17 +14,17 @@ import alertBtn from '../../Component/Common/Alert';
 
 export default function StudentSaving() {
   const navigate = useNavigate();
-  const savingList = useSelector(savingSelector);
   const studentSaving = useSelector(studentSavingSelector);
-  const { initStudentSavingList, changeStudentSavingList } = useStudentSaving();
+  const taxRateList = studentSaving[0].taxRateListGetResponses;
+  const { initStudentstudentSaving, changeStudentstudentSaving } =
+    useStudentSaving();
+
   // 학생 가입 정보 가져오기
-  const {
-    data, // eslint-disable-line no-unused-vars
-  } = useQuery({
+  useQuery({
     queryKey: ['studentSavingInfo'],
     queryFn: () =>
       getStudentSavingInfo().then((res) => {
-        initStudentSavingList(res.data);
+        initStudentstudentSaving(res.data);
         return res.data;
       }),
   });
@@ -40,7 +39,7 @@ export default function StudentSaving() {
       interestRate: wantedSaving.savingInterestRate,
     };
     if (alreadyApplied.length === 0) {
-      changeStudentSavingList(applySavingData.no);
+      changeStudentstudentSaving(applySavingData.no);
       applySaving.mutate(applySavingData);
     } else {
       alertBtn({
@@ -73,7 +72,7 @@ export default function StudentSaving() {
 
   return (
     <>
-      {savingList && studentSaving && (
+      {studentSaving && (
         <section className={styled.studentBank}>
           <img
             className={styled.back}
@@ -84,7 +83,7 @@ export default function StudentSaving() {
           <section className={styled.bankPage}>
             <section className={styled.bankSection}>
               <div className={styled.saveInfo}>
-                현재 국고에는 <span>{savingList[0].asset}비드</span> 있어요
+                현재 국고에는 <span>{studentSaving[0].asset}비드</span> 있어요
               </div>
               <div className={styled.saveTime}>
                 <span className={styled.saveTimeAlarm}>
@@ -95,6 +94,15 @@ export default function StudentSaving() {
                   이체될 때 가지고 있는 비드가 부족하면 적금은 자동으로 해지돼요
                 </span>
               </div>
+              <section className={styled.taxList}>
+                {taxRateList.map((tax) => (
+                  <section>
+                    <div className={styled.taxRate}>{tax.taxRate}%</div>
+                    <div className={styled.taxEnd}>{tax.endRange}</div>
+                    <div className={styled.taxIncome}>{tax.incomeLevel}</div>
+                  </section>
+                ))}
+              </section>
             </section>
             <section className={styled.savingView}>
               <div className={`${styled.savingA} ${styled.forStu}`}>
@@ -119,19 +127,19 @@ export default function StudentSaving() {
                 <section className={styled.inputArea}>
                   <div>
                     <span className={styled.title}>총 기간</span>
-                    <span>{savingList[0].savingDepositPeriod / 7}주</span>
+                    <span>{studentSaving[0].savingDepositPeriod / 7}주</span>
                   </div>
                   <div>
                     <span className={styled.title}>이체 주기</span>
-                    <span>{savingList[0].savingDepositCycle}일</span>
+                    <span>{studentSaving[0].savingDepositCycle}일</span>
                   </div>
                   <div>
                     <span className={styled.title}>입금 금액</span>
-                    <span>{savingList[0].savingDepositPrice}비드</span>
+                    <span>{studentSaving[0].savingDepositPrice}비드</span>
                   </div>
                   <div>
                     <span className={styled.title}>금리</span>
-                    <span>{savingList[0].savingInterestRate}%</span>
+                    <span>{studentSaving[0].savingInterestRate}%</span>
                   </div>
                 </section>
               </div>
@@ -159,19 +167,19 @@ export default function StudentSaving() {
                 <section className={styled.inputArea}>
                   <div>
                     <span className={styled.title}>총 기간</span>
-                    <span>{savingList[1].savingDepositPeriod / 7}주</span>
+                    <span>{studentSaving[1].savingDepositPeriod / 7}주</span>
                   </div>
                   <div>
                     <span className={styled.title}>이체 주기</span>
-                    <span>{savingList[1].savingDepositCycle}일</span>
+                    <span>{studentSaving[1].savingDepositCycle}일</span>
                   </div>
                   <div>
                     <span className={styled.title}>입금 금액</span>
-                    <span>{savingList[1].savingDepositPrice}비드</span>
+                    <span>{studentSaving[1].savingDepositPrice}비드</span>
                   </div>
                   <div>
                     <span className={styled.title}>금리</span>
-                    <span>{savingList[1].savingInterestRate}%</span>
+                    <span>{studentSaving[1].savingInterestRate}%</span>
                   </div>
                 </section>
               </div>
