26cb5691134cc24e3a548d07605a8c6aaef70284
olrlobt <317tmdgjs@naver.com>
2024-02-15 12:23:17 +0900

    [S10P12A306-877] fix: Modify updateGradePeriod

--- BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/board/repository/BoardRepository.java
+++ BackEnd/bid/bid-admin/src/main/java/com/ssafy/bid/domain/board/repository/BoardRepository.java
@@ -1,8 +1,15 @@
 package com.ssafy.bid.domain.board.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.bid.domain.board.Board;
 
+import jakarta.validation.constraints.NotNull;
+
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardCustomRepository {
+
+	List<Board> findAllByGradePeriodNo(@NotNull Integer gradePeriodNo);
+
 }
