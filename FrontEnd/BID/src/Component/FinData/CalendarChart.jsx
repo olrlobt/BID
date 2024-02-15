0b694f642c6abb8c931b92bdc9dfd6b8f12c91f1
김예림 <kimyelim100@naver.com>
2024-02-15 23:25:57 +0900

    [S10P12A306-801] feat: connect student calendar
    Former-commit-id: 93e363399b4ce9d18c8c3caf9f37c9464ed809b7

--- FrontEnd/BID/src/Component/FinData/CalendarChart.jsx
+++ FrontEnd/BID/src/Component/FinData/CalendarChart.jsx
@@ -1,25 +1,26 @@
 // CalendarChart.js
-import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
-import styled from './CalendarChart.module.css';
-import { Calendar, Navigate, momentLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment from 'moment';
-import Toolbar from './ToolBar';
-import CustomEvent from './CustomEvent';
-import monthDate from './monthDate';
-import monthHeader from './monthHeader';
-import SmallEvent from './SmallEvent';
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import styled from "./CalendarChart.module.css";
+import { Calendar, Navigate, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import Toolbar from "./ToolBar";
+import CustomEvent from "./CustomEvent";
+import monthDate from "./monthDate";
+import monthHeader from "./monthHeader";
+import SmallEvent from "./SmallEvent";
 
-function CalendarChart() {
-  moment.locale('ko-KR');
+function CalendarChart({ event }) {
+  console.log(event);
+  moment.locale("ko-KR");
   const localizer = momentLocalizer(moment);
 
   function MyDay({
     date,
     localizer,
     onDayClick,
-    scrollToTime = localizer.startOf(new Date(), 'day'),
+    scrollToTime = localizer.startOf(new Date(), "day"),
     ...props
   }) {
     return (
@@ -40,10 +41,10 @@ function CalendarChart() {
   MyDay.navigate = (date, action, { localizer }) => {
     switch (action) {
       case Navigate.PREVIOUS:
-        return localizer.add(date, -3, 'day');
+        return localizer.add(date, -3, "day");
 
       case Navigate.NEXT:
-        return localizer.add(date, 3, 'day');
+        return localizer.add(date, 3, "day");
 
       default:
         return date;
@@ -53,6 +54,7 @@ function CalendarChart() {
   MyDay.title = (date) => {
     return `My awesome week: ${date.toLocaleDateString()}`;
   };
+
   const { views } = useMemo(
     () => ({
       views: {
@@ -63,64 +65,11 @@ function CalendarChart() {
     []
   );
 
-  const events = [
-    {
-      id: 0,
-      title: '지출',
-      start: new Date(2024, 0, 15),
-      end: new Date(2024, 0, 15),
-      amount: -50,
-    },
-    {
-      id: 1,
-      title: '지출',
-      start: new Date(2024, 0, 26),
-      end: new Date(2024, 0, 26),
-      amount: -100,
-    },
-    {
-      id: 2,
-      title: '수입',
-      start: new Date(2024, 0, 15),
-      end: new Date(2024, 0, 15),
-      amount: 200,
-    },
-    {
-      id: 3,
-      title: '수입',
-      start: new Date(2024, 0, 1),
-      end: new Date(2024, 0, 1),
-      amount: 200,
-    },
-    {
-      id: 4,
-      title: '수입',
-      start: new Date(2024, 0, 8),
-      end: new Date(2024, 0, 8),
-      amount: 200,
-    },
-    {
-      id: 5,
-      title: '수입',
-      start: new Date(2024, 0, 22),
-      end: new Date(2024, 0, 22),
-      amount: 200,
-    },
-    {
-      id: 6,
-      title: '수입',
-      start: new Date(2024, 0, 29),
-      end: new Date(2024, 0, 29),
-      amount: 180,
-    },
-    // Add more events as needed
-  ];
-
   const calculateTotalAmount = (events, type) => {
     return events.reduce((total, event) => {
-      if (type === '지출') {
+      if (type === "지출") {
         return event.amount < 0 ? total + event.amount : total;
-      } else if (type === '수입') {
+      } else if (type === "수입") {
         return event.amount > 0 ? total + event.amount : total;
       }
       return total;
@@ -132,14 +81,14 @@ function CalendarChart() {
 
   const eventStyleGetter = (event, start, end, isSelected) => {
     const style = {
-      borderRadius: '0px',
+      borderRadius: "0px",
       opacity: 0.8,
-      color: 'white',
-      border: '0px',
-      display: 'block',
-      backgroundColor: 'transparent', // Set background color to transparent
-      width: '100%', // Make the event fill the entire day cell
-      cursor: 'pointer', // Enable cursor for events
+      color: "white",
+      border: "0px",
+      display: "block",
+      backgroundColor: "transparent", // Set background color to transparent
+      width: "100%", // Make the event fill the entire day cell
+      cursor: "pointer", // Enable cursor for events
     };
 
     const content = (
@@ -157,7 +106,7 @@ function CalendarChart() {
     <div className={styled.chartContainer}>
       <Calendar
         localizer={localizer}
-        events={events}
+        events={event}
         defaultDate={moment().toDate()}
         startAccessor="start"
         endAccessor="end"
