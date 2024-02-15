36350eed422f130b267aa27eb1b6509d51594b6a
배민지 <bae1997@naver.com>
2024-02-15 18:18:17 +0900

    fix: Add student password update

--- BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/user/service/UserServiceImpl.java
+++ BackEnd/bid/bid-student/src/main/java/com/ssafy/bid/domain/user/service/UserServiceImpl.java
@@ -2,14 +2,17 @@ package com.ssafy.bid.domain.user.service;
 
 import java.time.LocalDate;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.bid.domain.user.Admin;
 import com.ssafy.bid.domain.user.Attendance;
 import com.ssafy.bid.domain.user.Student;
 import com.ssafy.bid.domain.user.User;
 import com.ssafy.bid.domain.user.UserType;
 import com.ssafy.bid.domain.user.dto.CustomUserInfo;
+import com.ssafy.bid.domain.user.dto.StudentPasswordUpdateRequest;
 import com.ssafy.bid.domain.user.repository.UserRepository;
 import com.ssafy.bid.global.error.exception.AuthorizationFailedException;
 import com.ssafy.bid.global.error.exception.InvalidParameterException;
@@ -23,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	@Transactional
@@ -59,5 +63,27 @@ public class UserServiceImpl implements UserService {
 		}
 
 		return result;
+	}
+
+	@Override
+	@Transactional
+	public void updatePassword(CustomUserInfo userInfo, StudentPasswordUpdateRequest request) {
+		User user = userRepository.findById(userInfo.getNo())
+			.orElseThrow(() -> new ResourceNotFoundException("학생패스워드변경: 패스워드 수정하려는 Student 엔티티가 없음.", userInfo.getNo()));
+
+		if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+			throw new InvalidParameterException("학생패스워드변경: 현재 패스워드와 기입한 패스워드 불일치.", request.getCurrentPassword());
+		}
+
+		if (!request.getNewPassword().equals(request.getNewPasswordCheck())) {
+			throw new InvalidParameterException("학생패스워드변경: 패스워드와 패스워드 확인 불일치.", request.getNewPassword());
+		}
+
+		if (user instanceof Student student) {
+			student.changePassword(passwordEncoder.encode(request.getNewPassword()));
+			return;
+		}
+
+		throw new AuthorizationFailedException("학생 권한이 아님.");
 	}
 }
