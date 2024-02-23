c5bced1e876740baa391ac6b86ba2a444c82f4bf
김예림 <kimyelim100@naver.com>
2024-02-23 17:54:44 +0900

    change: StudentFinData
    Former-commit-id: f430a917e0c37f143399066f2033d95a64b25c11

--- FrontEnd/BID/src/Component/Manage/Student.jsx
+++ FrontEnd/BID/src/Component/Manage/Student.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import styled from "./Student.module.css";
 import useModal from "../../hooks/useModal";
-import PwdRemoveBtn from './PwdRemoveBtn';
-import DoDisturbOnIcon from '@mui/icons-material/DoDisturbOn';
+import PwdRemoveBtn from "./PwdRemoveBtn";
+import DoDisturbOnIcon from "@mui/icons-material/DoDisturbOn";
 import { SvgIcon } from "@material-ui/core";
-import EditIcon from '@mui/icons-material/Edit';
-import { deleteStudentApi } from "../../Apis/UserApis";
+import EditIcon from "@mui/icons-material/Edit";
 import { useMutation } from "@tanstack/react-query";
-import { useSelector } from "react-redux";
-import { mainSelector } from "../../Store/mainSlice";
 
 const Student = ({ item, onClick, handleRemove, handleEdit, showRemove }) => {
-  
-  const mainClass = useSelector(mainSelector);
   /** 학생 비밀번호 초기화 쿼리 */
   const resetPwdQuery = useMutation({
-    mutationKey: ['resetPwd'],
+    mutationKey: ["resetPwd"],
     mutationFn: () => resetPwdApi(),
     onSuccess: (res) => {
       console.log(res.data);
@@ -29,7 +24,7 @@ const Student = ({ item, onClick, handleRemove, handleEdit, showRemove }) => {
   const resetPwdApi = (e) => {
     e.preventDefault();
     resetPwdQuery.mutate();
-  }
+  };
 
   // /** 학생 제거 쿼리 */
   // const deleteStudentQuery = useMutation({
@@ -45,8 +40,8 @@ const Student = ({ item, onClick, handleRemove, handleEdit, showRemove }) => {
 
   /** 학생 비밀번호 초기화 **/
   const onRemove = () => {
-    handleRemove(item.no)
-    console.log(item)
+    handleRemove(item.no);
+    console.log(item);
     // deleteStudentQuery.mutate(item.no,mainClass.no)
   };
 
@@ -56,7 +51,6 @@ const Student = ({ item, onClick, handleRemove, handleEdit, showRemove }) => {
 
   const { openModal } = useModal();
 
-
   return (
     <>
       <tr className={styled.table} onClick={onClick}>
@@ -64,31 +58,37 @@ const Student = ({ item, onClick, handleRemove, handleEdit, showRemove }) => {
         <td>{item.name}</td>
         <td>{item.asset}</td>
         {showRemove && (
-          <td className={styled.resetPwdCell} onClick={onEdit}>              <PwdRemoveBtn
-                onClick={() =>
-                  openModal({
-                    type: "pwdRemove",
-                    props: ["비밀번호 초기화", item]
-                  })
-                }
-                />
+          <td className={styled.resetPwdCell} onClick={onEdit}>
+            {" "}
+            <PwdRemoveBtn
+              onClick={() =>
+                openModal({
+                  type: "pwdRemove",
+                  props: ["비밀번호 초기화", item],
+                })
+              }
+            />
           </td>
         )}
         {showRemove && (
           <td onClick={onRemove}>
-            <SvgIcon component={DoDisturbOnIcon} style={{ fill: "red", height: "3vh" }}  />
+            <SvgIcon
+              component={DoDisturbOnIcon}
+              style={{ fill: "red", height: "3vh" }}
+            />
           </td>
         )}
         {showRemove && (
           <td onClick={onEdit}>
-              <SvgIcon component={EditIcon}
-                onClick={() =>
-                  openModal({
-                    type: "editStudent",
-                    props: ["학생 정보 수정", item]
-                  })
-                }
-                />
+            <SvgIcon
+              component={EditIcon}
+              onClick={() =>
+                openModal({
+                  type: "editStudent",
+                  props: ["학생 정보 수정", item],
+                })
+              }
+            />
           </td>
         )}
       </tr>
--- FrontEnd/BID/src/Page/Manage/ClassPage.jsx
+++ FrontEnd/BID/src/Page/Manage/ClassPage.jsx
@@ -1,49 +1,87 @@
 import React, { useEffect, useState } from "react";
 import styled from "./ClassPage.module.css";
-import StudentList from '../../Component/Manage/StudentList'
-import StudentFinData from '../../Component/Manage/StudentFinData';
+import StudentList from "../../Component/Manage/StudentList";
+import StudentFinData from "../../Component/Manage/StudentFinData";
 import Button from "../../Component/Common/Button";
 import Button2 from "../../Component/Common/Button2";
 import Button3 from "../../Component/Common/Button3";
 import useModal from "../../hooks/useModal";
-import { getStudentListApi  } from "../../Apis/TeacherManageApis";
+import {
+  getStudentListApi,
+  viewStudentDetail,
+} from "../../Apis/TeacherManageApis";
 import { deleteStudentApi } from "../../Apis/UserApis";
 import { useSelector } from "react-redux";
-import { studentSelector  } from "../../Store/studentSlice";
-import  useStudents  from "../../hooks/useStudents";
-import { useQuery } from "@tanstack/react-query";
+import { studentSelector } from "../../Store/studentSlice";
+import useStudents from "../../hooks/useStudents";
+import { useQueries, useQuery, useQueryClient } from "@tanstack/react-query";
 import { mainSelector } from "../../Store/mainSlice";
 
 function ClassPage() {
   const [studentList, setStudentList] = useState([]);
-  const [activeButton, setActiveButton] = useState('number');
-  const [sortType, setSortType] = useState('number');
+  const [activeButton, setActiveButton] = useState("number");
+  const [sortType, setSortType] = useState("number");
   const [showAdd, setShowAdd] = useState(false);
   const [showRemove, setShowRemove] = useState(false);
   const [selectedStudent, setSelectedStudent] = useState(null);
+  const [studentData, setStudentData] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const mainClass = useSelector(mainSelector);
 
   const gradeNo = mainClass.no;
   const { initStudents } = useStudents();
   const students = useSelector(studentSelector);
-  console.log(students)
+  const queryClient = useQueryClient();
+
   const { openModal } = useModal();
-
   useEffect(() => {
-    fetchStudentList();
-  }, [showAdd, onclose]);
+    console.log(studentList);
+    const fetchStudentData = async () => {
+      if (studentList.length > 0) {
+        const queries = studentList.map((info) => ({
+          queryKey: ["data", info.no],
+          queryFn: async () => {
+            try {
+              const res = await viewStudentDetail(
+                gradeNo,
+                info.no,
+                "2024-02-01",
+                "2024-02-28"
+              );
+              return { no: info.no, data: res.data };
+            } catch (error) {
+              console.error(error);
+            }
+          },
+        }));
+        const results = await Promise.allSettled(
+          queries.map(({ queryFn }) => queryFn())
+        );
 
-  const fetchStudentList = () => {
-    getStudentListApi(gradeNo).then((res) => {
-      if (res.data !== undefined) {
-        setStudentList(res.data);
-        initStudents(res.data );
+        const newData = results
+          .filter((result) => result.status === "fulfilled")
+          .map((result) => result.value);
+
+        setStudentData(newData);
       }
-    }).catch(error => {
-      console.error("학생 목록을 불러오는 중 오류가 발생했습니다:", error);
-    });
-  };
+    };
+    fetchStudentData();
+  }, [showAdd, onclose, studentList]);
+
+  useQuery({
+    queryKey: ["groupInfo"],
+    queryFn: () =>
+      getStudentListApi(gradeNo)
+        .then(async (res) => {
+          if (res.data !== undefined) {
+            setStudentList(res.data);
+            initStudents(res.data);
+          }
+        })
+        .catch((error) => {
+          console.error("학생 목록을 불러오는 중 오류가 발생했습니다:", error);
+        }),
+  });
 
   const handleStudentClick = (student) => {
     if (!isEditing) {
@@ -54,11 +92,13 @@ function ClassPage() {
   };
 
   useEffect(() => {
-    const firstStudentWithNumberOne = studentList.find(student => student.number === 1);
+    const firstStudentWithNumberOne = studentList.find(
+      (student) => student.number === 1
+    );
     if (firstStudentWithNumberOne) {
       setSelectedStudent(firstStudentWithNumberOne);
     } else {
-      setSelectedStudent(studentList[0])
+      setSelectedStudent(studentList[0]);
     }
   }, [studentList]);
 
@@ -73,13 +113,15 @@ function ClassPage() {
   };
 
   const handleRemove = (no) => {
-    console.log(no)
-    deleteStudentApi(no, gradeNo).then((res) => {
-      console.log(res);
-      fetchStudentList();
-    }).catch(error => {
-      console.error("학생 삭제 중 오류가 발생했습니다:", error);
-    });
+    console.log(no);
+    deleteStudentApi(no, gradeNo)
+      .then((res) => {
+        console.log(res);
+        // fetchStudentList();
+      })
+      .catch((error) => {
+        console.error("학생 삭제 중 오류가 발생했습니다:", error);
+      });
   };
 
   const handleSort = (type) => {
@@ -87,16 +129,18 @@ function ClassPage() {
     setSortType(type);
   };
 
-  const sortedInfo = studentList && [...studentList].sort((a, b) => {
-    if (sortType === 'number') {
-      return a.number - b.number;
-    } else if (sortType === 'asset') {
-      const assetA = parseInt(a.asset);
-      const assetB = parseInt(b.asset);
-      return assetB - assetA;
-    }
-    return 0;
-  });
+  const sortedInfo =
+    studentList &&
+    [...studentList].sort((a, b) => {
+      if (sortType === "number") {
+        return a.number - b.number;
+      } else if (sortType === "asset") {
+        const assetA = parseInt(a.asset);
+        const assetB = parseInt(b.asset);
+        return assetB - assetA;
+      }
+      return 0;
+    });
 
   const handleAddStudentComplete = () => {
     setIsEditing(true); // 학생 추가 후 편집 모드로 전환
@@ -109,32 +153,30 @@ function ClassPage() {
           <Button3
             text="학생 추가"
             onClick={() =>
-              openModal({ 
-              type: "addStudent", 
-              props: ["학생 등록", handleAddStudentComplete],
+              openModal({
+                type: "addStudent",
+                props: ["학생 등록", handleAddStudentComplete],
               })
             }
           />
         )}
         <div className={styled.buttonSpacing} />
-        <Button2
-          text="학생 편집"
-          onClick={handleButton2Click}
-        />
+        <Button2 text="학생 편집" onClick={handleButton2Click} />
       </div>
       <div className={styled.orderlist}>
         <Button
           text="번호"
-          onClick={() => handleSort('number')}
-          active={activeButton === 'number'}
+          onClick={() => handleSort("number")}
+          active={activeButton === "number"}
         />
         <Button
           text="총 비드"
-          onClick={() => handleSort('asset')}
-          active={activeButton === 'asset'}
+          onClick={() => handleSort("asset")}
+          active={activeButton === "asset"}
         />
       </div>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
+        {console.log(studentData)}
         <div className={styled.orderTableContainer}>
           <table className={styled.orderTable}>
             <thead>
@@ -161,9 +203,14 @@ function ClassPage() {
             />
           </table>
         </div>
-        {selectedStudent && !isEditing && (
-          <div className={`${styled.studentFinDataContainer} ${styled.studentFinDataTable}`}>
-            <StudentFinData student={selectedStudent} />
+        {selectedStudent && !isEditing && studentData && (
+          <div
+            className={`${styled.studentFinDataContainer} ${styled.studentFinDataTable}`}
+          >
+            <StudentFinData
+              student={selectedStudent}
+              studentData={studentData.find((v) => v.no === selectedStudent.no)}
+            />
           </div>
         )}
       </div>
